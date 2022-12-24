0453de80e62a70c438fff08dac64801b9b6c8992
hengel70 <hengel70@gmail.com>
2022-12-24 20:53:53 +0000

    Assignment submission 1

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/File_Name.js
@@ -0,0 +1,9 @@
+window.alert("hello again"); //an alert pops up
+document.write("Hello, world! <br>"+"This is a second line <br>");//writing on doc
+var a="This is \"a\" String", b="second var" //create 2 vars
+document.write(a);
+window.alert(a); 
+var blues="I have a blues"
+var blues= blues.fontcolor("blue");//change color
+document.write(blues);
+document.write(3+3);
